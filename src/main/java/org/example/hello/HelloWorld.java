e83916b261f4f63b20f18fa2e351dd9ee43a50d9
ntbolton <ntbolton03@gmail.com>
2023-08-29 18:50:18 -0700

    adding hello world

---
+++ src/main/java/org/example/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.example.hello;
+
+
+public class HelloWorld {
+
+    // shortcut -> psvm
+    public static void main(String[] args)
+    {
+        //shortcut -> sout
+        System.out.println("Hello World!");
+    }
+}
